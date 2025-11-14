69027280deabf14dc8c7aba9ef21de97115de8ca
Kai Fett <kf@schnism.net>
2025-11-14 01:25:05 +0100

    Var scope

--- coords.js
+++ coords.js
@@ -7,42 +7,45 @@ function handleTextChange(event) {
 
     try {
 
+	var nx,ny,nz,ox,oy,oz;
+
         coords = value.match(/([-\d]+)[^-\d]+([-\d]+)[^-\d]+([-\d]+)/);
         console.log(coords);
         console.log(mode);
 
         if (mode === 'nether') {
-            let nx = coords[1];
-            let ny = coords[2];
-            let nz = coords[3];
+            nx = coords[1];
+            ny = coords[2];
+            nz = coords[3];
 
-            let ox = Math.round(nx * 8);
-            let oy = ny;
-            let oz = Math.round(nz * 8);
+            ox = Math.round(nx * 8);
+            oy = ny;
+            oz = Math.round(nz * 8);
 
-            document.getElementById('overworldt').innerText = `x=${ox} y=${oy} z=${oz}`;
-            document.getElementById('nethert').innerText = `x=${nx} y=${ny} z=${nz}`;
             document.getElementById('overworld').value = `${ox} ${oy} ${oz}`;
-            document.getElementById('omap').src = `https://maps.schnism.net/#overworld:${ox}:${oy}:${oz}:200:0:0:0:1:flat`;
-            document.getElementById('nmap').src = `https://maps.schnism.net/#nether:${nx}:${ny}:${nz}:200:0:0:0:1:flat`;
+
+
 
         }
 
         if (mode === 'overworld') {
-            let ox = coords[1];
-            let oy = coords[2];
-            let oz = coords[3];
+            ox = coords[1];
+            oy = coords[2];
+            oz = coords[3];
 
-            let nx = Math.round(ox / 8);
-            let ny = oy;
-            let nz = Math.round(oz / 8);
+            nx = Math.round(ox / 8);
+            ny = oy;
+            nz = Math.round(oz / 8);
+            document.getElementById('nether').value = `${nx} ${ny} ${nz}`;
+
+        }
+
+
             document.getElementById('overworldt').innerText = `x=${ox} y=${oy} z=${oz}`;
             document.getElementById('nethert').innerText = `x=${nx} y=${ny} z=${nz}`;
-            document.getElementById('nether').value = `${nx} ${ny} ${nz}`;
             document.getElementById('omap').src = `https://maps.schnism.net/#overworld:${ox}:${oy}:${oz}:200:0:0:0:1:flat`;
             document.getElementById('nmap').src = `https://maps.schnism.net/#nether:${nx}:${ny}:${nz}:200:0:0:0:1:flat`;
 
-        }
 
     } catch (e) {
         document.getElementById('overworldt').innerText = ``;
